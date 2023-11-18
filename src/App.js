034b888359326278ebedd7bdcd1d5153e7e8cd31
Inder-koundal <137680358+Inder-koundal@users.noreply.github.com>
2023-11-18 15:24:36 +0530

    adding project tree

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect,useState } from 'react';
+import twitterLogo from './assets/twitter.png'
 function App() {
+  const [quoteInfo, setQuoteInfo] = useState({});
+  const [isLoading, setIsloading] = useState(false)
+  const [appColor, setAppColor] = useState('skyblue')
+
+  useEffect(()=>{
+    getQuote();
+    setIsloading(true)
+  },[])
+
+  const getQuote = ()=>{
+    fetch("https://api.quotable.io/random")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) =>{
+      setQuoteInfo({
+        text:data.content,
+        author:data.author,
+      })
+      setIsloading(false)
+      const randomColor = Math.floor(Math.random()*16777215).toString(16);
+      setAppColor(`#${randomColor}`)
+    });
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{background: appColor, margin: 0, padding: 0, height: '100vh'}}>
+      <div className='quote-wrpper' style={{background: appColor}}>
+        <div id="quote-box" style={{width:'500px',margin:'0 auto'}}>
+          {
+            !isLoading ? (
+              <>
+                <p id="text" style={{color: appColor}}>{`“${quoteInfo.text}`}</p>
+                <p id="author" style={{color: appColor}}>{`:- ${quoteInfo.author}`}</p>
+              </>
+            ) :
+            <div className='loader-wrapper'>
+              <div className='loading'>
+              </div>
+            </div>
+          }
+          <div className='action-buttons'>
+            <button 
+              style={{background: appColor}}
+              id="new-quote" 
+              onClick={() => {
+                getQuote()
+                setIsloading(true)
+              }}
+            >New Quote</button>
+            <a href={'https://twitter.com/intent/tweet'+quoteInfo.text} id="tweet-quote">
+              <img src={twitterLogo} alt='twitter' className='twitter-icon' />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
